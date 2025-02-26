63f8034352da05569e160ea5af6fdbeb4709f178
abhisheksh99 <abhishekleo1234@gmail.com>
2025-02-26 15:01:54 -0500

    Design Backend implemeted with mongodb

--- custom-thread-backend/src/routes/design.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { designController } from "../controllers/design.controller";
-
-const router = Router();
-
-router.post("/", designController.createDesign);
-router.get("/", designController.getDesigns);
-
-export default router;
