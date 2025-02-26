d43f0b35a4707e41562ec26814676fdea84cadf8
BeLazy167 <dhruvkhara167@gmail.com>
2025-02-26 17:15:11 -0500

    chore(backend): update dependencies and entry point

--- custom-thread-backend/src/index.ts
+++ custom-thread-backend/src/index.ts
@@ -2,16 +2,19 @@ import express from "express";
 import designRoutes from "./routes/designRoute";
 import { errorMiddleware } from "./middlewares/error.middleware";
 import connectDb from "./config/database";
-
-
-
+import cors from "cors";
 const app = express();
 const port = process.env.PORT || 3001;
 connectDb();
 
 app.use(express.json());
 
-
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        credentials: true,
+    })
+);
 // Routes
 app.use("/api/v1/designs", designRoutes);
 
@@ -19,5 +22,5 @@ app.use("/api/v1/designs", designRoutes);
 app.use(errorMiddleware);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
