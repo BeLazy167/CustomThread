24bc73c5eaf9c2c3b19240d6cfda4a022b2192dd
Abhishek Sharma <158095474+abhisheksh99@users.noreply.github.com>
2025-02-27 21:26:14 -0500

    Update main.tsx

--- custom-thread-frontend/src/main.tsx
+++ custom-thread-frontend/src/main.tsx
@@ -8,8 +8,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/query-client";
 
-//const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-const PUBLISHABLE_KEY = "pk_test_YXdhaXRlZC1mZWxpbmUtMjcuY2xlcmsuYWNjb3VudHMuZGV2JA"
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+// const PUBLISHABLE_KEY = "pk_test_YXdhaXRlZC1mZWxpbmUtMjcuY2xlcmsuYWNjb3VudHMuZGV2JA"
 
 
 if (!PUBLISHABLE_KEY) {
