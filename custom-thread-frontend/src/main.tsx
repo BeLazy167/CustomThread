610d3126c477c4729ab8f33f8089a6319d47a7c6
BeLazy167 <dhruvkhara167@gmail.com>
2025-02-27 23:00:06 -0500

    Fix TypeScript errors in crypto.randomUUID polyfill

--- custom-thread-frontend/src/main.tsx
+++ custom-thread-frontend/src/main.tsx
@@ -13,11 +13,19 @@ const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 // Add polyfill for crypto.randomUUID
 if (!crypto.randomUUID) {
-  crypto.randomUUID = function() {
-    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-      (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-  };
+    crypto.randomUUID = function () {
+        return "10000000-1000-4000-8000-100000000000".replace(
+            /[018]/g,
+            (c: string) => {
+                const num = Number(c);
+                return (
+                    num ^
+                    (crypto.getRandomValues(new Uint8Array(1))[0] &
+                        (15 >> (num / 4)))
+                ).toString(16);
+            }
+        );
+    };
 }
 
 if (!PUBLISHABLE_KEY) {
