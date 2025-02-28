7bae43a25232e85545bfe814a843432e0155d79e
Abhishek Sharma <158095474+abhisheksh99@users.noreply.github.com>
2025-02-27 20:21:38 -0500

    testing change main.tsx

--- custom-thread-frontend/src/main.tsx
+++ custom-thread-frontend/src/main.tsx
@@ -8,7 +8,9 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/query-client";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+//const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+const PUBLISHABLE_KEY = "pk_test_YXdhaXRlZC1mZWxpbmUtMjcuY2xlcmsuYWNjb3VudHMuZGV2JA"
+
 
 if (!PUBLISHABLE_KEY) {
     throw new Error("Missing Publishable Key");
